db3e9848c4cff204dba236a9772acd9dc20f3789
DmitriyBalaba <d.balaba@jazzserve.com>
2024-03-19 15:30:40 +0200

    test relate

--- src/relations/postgresql_provider.py
+++ src/relations/postgresql_provider.py
@@ -138,12 +138,12 @@ class PostgreSQLProvider(Object):
     def _on_relation_changed_event(self, event: RelationChangedEvent) -> None:
         """Event emitted when the relation has changed."""
         # Leader only
-        if not self.local_unit.is_leader():
+        if not self.charm.unit.is_leader():
             return
         logger.info(f" provide_psql --------------  len = {self.relation_name}")
         for relation in self.charm.client_relations:
             if self.relation_name != relation.name:
-                self.local_unit.status = BlockedStatus(ENDPOINT_SIMULTANEOUSLY_BLOCKING_MESSAGE)
+                self.charm.unit.status = BlockedStatus(ENDPOINT_SIMULTANEOUSLY_BLOCKING_MESSAGE)
                 logger.info(
                     f" provide_psql --------------  local_unit.status  = {ENDPOINT_SIMULTANEOUSLY_BLOCKING_MESSAGE}")
                 return
