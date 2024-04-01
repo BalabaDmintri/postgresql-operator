46be7d591760e6659f6dd1c05dc7f71f7f637183
DmitriyBalaba <d.balaba@jazzserve.com>
2024-04-01 14:56:09 +0300

    test storage

--- tests/integration/ha_tests/test_self_healing.py
+++ tests/integration/ha_tests/test_self_healing.py
@@ -571,7 +571,7 @@ async def test_deploy_zero_units(ops_test: OpsTest):
         )
 
     async with ops_test.fast_forward():
-        await ops_test.model.wait_for_idle(apps=["psql-first", "psql-second", ], status="active", timeout=1500)
+        await ops_test.model.wait_for_idle(apps=["psql-first"], status="active", timeout=1500)
 
     # unit_storage_id = ""
     # for unit in ops_test.model.applications["psql-second"].units:
