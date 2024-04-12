0b1891f04d8bb7ff0b47f2828a6a59728baece96
DmitriyBalaba <d.balaba@jazzserve.com>
2024-04-12 09:31:29 +0300

    fix check connect to legacy endpoint

--- src/charm.py
+++ src/charm.py
@@ -719,6 +719,7 @@ class PostgresqlOperatorCharm(TypedCharmBase[CharmConfig]):
     def members_ips(self) -> Set[str]:
         """Returns the list of IPs addresses of the current members of the cluster."""
         logger.info(f" ------------- 11111-= {self._peers.data[self.app]}")
+        logger.info(f" ------------- 22222-= {self._peers.data[self.unit]}")
         return set(json.loads(self._peers.data[self.app].get("members_ips", "[]")))
 
     def _add_to_members_ips(self, ip: str) -> None:
