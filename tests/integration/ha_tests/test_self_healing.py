fddf4fe58ec46d3251eddff7e282310906f1c6d1
DmitriyBalaba <d.balaba@jazzserve.com>
2024-04-17 18:27:52 +0300

    deploy postgresql-test-app with latest version

--- tests/integration/ha_tests/test_self_healing.py
+++ tests/integration/ha_tests/test_self_healing.py
@@ -87,6 +87,7 @@ async def test_build_and_deploy(ops_test: OpsTest) -> None:
                 application_name=APPLICATION_NAME,
                 series=CHARM_SERIES,
                 channel="edge",
+                revision=103,
             )
 
     if wait_for_apps:
